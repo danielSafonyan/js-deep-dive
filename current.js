235856d8f1722954e36dea225cefa906d4e111a8
danielSafonyan <danielsafonyan@gmail.com>
2023-01-06 12:37:42 +0000

    Leanred about constructor functions

--- current.js
+++ current.js
@@ -1,26 +1,15 @@
-// Challenge: 
-// Use reduce() to sum the weights of all the cars
-// Stretch goal: 
-// Use reduce to sum the weights of only the electric cars 
-
-const cars = [
-  { name: "Toyota", isElectric: false, weight: 1320 },
-  { name: "Ford", isElectric: false, weight: 1400 },
-  { name: "Volkswagen", isElectric: false, weight: 1370 },
-  { name: "Honda", isElectric: false, weight: 1375 },
-  { name: "Tesla", isElectric: true, weight: 1750 },
-  { name: "BMW", isElectric: true, weight: 1350 },  
-];
-
-const allCarWeightSum = cars.reduce((accum, el) => accum + el.weight, 0)
-console.log("allCarWeightSum", allCarWeightSum)
-
-const eCarWeightSum = cars.reduce((accum, cur) => cur.isElectric ? cur.weight : 0, 0)
-console.log("eCarWeightSum", eCarWeightSum)
+function Book(id, title, author, themes = []) {
+	this.id = id, 
+	this.title = title, 
+	this.author = author, 
+	this.themes = themes
+}
 
 
-// With .reduce you can also create new arrays
-const numbers = [1, 2, 3, 4, 5, 6];
+Book.prototype.addTheme = function (theme) {
+    this.themes.push(theme)
+}
 
-const doubledArray = numbers.reduce((accum, cur) => [...accum, cur * 2], [])
-console.log("doubledArray", doubledArray)
+const myBook = new Book(1, 'Harry Potter', 'JK ROwling', ['Fantasy', 'Horror'])
+
+myBook.addTheme('Awesome Theme')
